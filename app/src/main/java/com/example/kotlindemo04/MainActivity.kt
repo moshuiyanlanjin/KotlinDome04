1257a640b5b33233331652ca8ada79b246257d27
moshuiyanlanjin <1078314344@qq.com>
2023-05-07 22:04:03 +0800

    1

--- app/src/main/java/com/example/kotlindemo04/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo04/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlindemo04
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import com.example.kotlindemo04.ui.BaseActivity
 
@@ -8,6 +9,8 @@ import com.example.kotlindemo04.ui.BaseActivity
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d("zlc","111")
 
     }
 }
